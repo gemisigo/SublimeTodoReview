e4a8c9e84ed2df4122882e327001334d42e732fa
Misi Gerstmajer <gemisigo@gmail.com>
2022-11-16 15:34:25 +0100

    [FIX] version pattern no longer contains a dash

--- patterns.py
+++ patterns.py
@@ -10,7 +10,7 @@ import re
 from TodoReview.types import TYPES
 
 VERSION_LIST_PATTERN = "(?:-- #region\s*\**\sversions start\s*\**?\n)(?P<versions>.*?)(?:-- #endregion\s*\**\sversions end\s*\**)"
-VERSION_ENTRY_PATTERN = "^-- (?P<date>.{20})\s*(?P<author>\w*)\s* - \((?P<version>.*?)\)\s+(?P<comment>.*)$"
+VERSION_ENTRY_PATTERN = "^-- (?P<date>.{20})\s*(?P<author>\w*)\s+\((?P<version>.*?)\)\s+(?P<comment>.*)$"
 VERSION_PATTERN = "^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<build>\d+)$"
 # OBJECT_PATTERN = f"^(?P<type>{'|'.join(TYPES.keys())})\.(?P<schema>.*?)\.(?P<object>.*?)\.sql$"
 OBJECT_PATTERN = f"^(?P<rubbish>.*?)(?P<type>{'|'.join(TYPES.keys())})\.(?P<schema>.*?)\.(?P<object>.*?)\.sql$"
